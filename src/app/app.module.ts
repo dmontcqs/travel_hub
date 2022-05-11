32a0452dd6e5b4cc8940f9a09ffca0e78482a1fc
diego1871 <46872050+diego1871@users.noreply.github.com>
2022-05-11 17:00:45 -0500

    cambios con NgFor, ngClass, ngStyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { LeftMenuComponent } from './left-menu/left-menu.component';
-import { ToolBarComponent } from './tool-bar/tool-bar.component';
 import { ContentComponent } from './content/content.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CurrencyPipe } from './currency.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LeftMenuComponent,
-    ToolBarComponent,
     ContentComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CurrencyPipe
   ],
   imports: [
     BrowserModule
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+// aquí cla app y la vista (html y css)sss
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
+
 })
+
+// aquí metés las funciones o la lógica del componente
+
 export class ContentComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  otrosDestinos: string[] = ["Cobá", "Chiapas", "Durango", "jalisco", "Monterrey"];
+  condition: boolean = true; 
+  hayLista: boolean = true; 
+  fecha:string = '10/09/2022';
+  price: number = 80.45; 
+  
 }
 
